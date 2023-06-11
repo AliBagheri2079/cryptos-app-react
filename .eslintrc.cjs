8bd73067a8a5b4b6005aa571d12ea4121f32fd7f
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-12 01:54:50 +0330

    :wrench: Chore: setup eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,15 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'standard-with-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
