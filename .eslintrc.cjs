24ce0515a70312d03dda97623846d3b356b465e8
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-12 02:42:58 +0330

    :wrench: Chore: update eslint configuration with eslint tailwind plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,10 @@
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'standard-with-typescript',
+    'plugin:tailwindcss/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
@@ -10,6 +12,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh', 'react'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js'],
+      parser: '@typescript-eslint/parser',
+    },
+  ],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
