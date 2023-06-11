ba63c46fac9d64f88ca04ada0a7a04f067c08d2f
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-12 02:33:19 +0330

    :wrench: Chore: update stylelint config with tailwind and order plugins

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,1 +1,19 @@
-module.exports = { extends: ['stylelint-config-standard'] };
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'order/order': ['custom-properties', 'declarations'],
+    'order/properties-order': ['width', 'height'],
+    'no-descending-specificity': null,
+    'at-rule-no-unknown': null,
+    'max-nesting-depth': 2,
+    'selector-max-id': 1,
+    'selector-class-pattern': null,
+  },
+  overrides: [],
+  reportNeedlessDisables: [],
+  reportDescriptionlessDisables: [],
+  reportInvalidScopeDisables: [],
+  configurationComment: [],
+  ignoreDisables: [],
+};
