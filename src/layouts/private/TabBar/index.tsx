a93c84b0ed8a84bec6c5aced30d142d6b8d6c66a
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-18 04:07:55 +0330

    :sparkles: Feat: create tabbar layout

---
+++ src/layouts/private/TabBar/index.tsx
@@ -0,0 +1,48 @@
+import { Group, Navbar, ScrollArea, useMantineTheme } from '@mantine/core'
+import { type FC } from 'react'
+
+import { LinkTab } from '@/components/Nav/Link'
+import { navLinkData } from '@/data/navLinkData'
+import { BASE_COL_HEIGHT } from '@/utils/constants/col-height'
+import { randomId } from '@/utils/helpers/random-id'
+
+const BASE_HEIGHT = `calc(${BASE_COL_HEIGHT.SECOND} * 1.8)`
+
+const TabBar: FC = () => {
+  const theme = useMantineTheme()
+
+  return (
+    <Navbar
+      fixed={true}
+      h={BASE_HEIGHT}
+      w='auto'
+      sx={{
+        border: 'unset',
+        insetInline: theme.spacing.xs
+      }}
+    >
+      <Navbar.Section component={ScrollArea} grow>
+        <Group h={BASE_HEIGHT} align='flex-end'>
+          <Group
+            spacing='xs'
+            align='flex-end'
+            sx={{
+              flexWrap: 'nowrap',
+              borderRadius: theme.spacing.xs,
+              backgroundColor:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[5]
+                  : theme.colors.gray[2]
+            }}
+          >
+            {navLinkData.map(items => (
+              <LinkTab key={randomId()} {...items} />
+            ))}
+          </Group>
+        </Group>
+      </Navbar.Section>
+    </Navbar>
+  )
+}
+
+export default TabBar
