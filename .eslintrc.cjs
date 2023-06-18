81bb821774781540d3c55479161dbfa24c94f6d2
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-18 04:13:43 +0330

    :wrench: Chore: update eslint configuration to solve lint problems, add many rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,41 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'standard-with-typescript',
-    'plugin:tailwindcss/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh', 'react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx', '*.js'],
       parser: '@typescript-eslint/parser',
     },
   ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'multiline-ternary': 'off',
+    semi: 'off',
+    '@typescript-eslint/semi': 'off',
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/prefer-optional-chain': 'off',
   },
 };
