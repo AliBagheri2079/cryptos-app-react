ad06cf135267d1b9a7d5c0aa605797241ca06a3c
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-18 03:57:05 +0330

    :lipstick: Style: create link minimal style file

---
+++ src/components/Nav/Link/Minimal/index.style.ts
@@ -0,0 +1,31 @@
+import { createStyles, rem } from '@mantine/core';
+
+export const useMinimalStyles = createStyles(theme => ({
+  link: {
+    width: rem(50),
+    height: rem(50),
+    borderRadius: theme.radius.md,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[5]
+          : theme.colors.gray[0],
+    },
+  },
+
+  active: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({
+        variant: 'filled',
+        color: theme.primaryColor,
+      }).background,
+      color: theme.fn.variant({ variant: 'filled', color: theme.primaryColor })
+        .color,
+    },
+  },
+}));
