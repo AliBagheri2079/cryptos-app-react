f0bfe1d6f77e5e15c7bfc12ee8a5c038525565da
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-22 03:18:56 +0330

    :sparkles: Feat: create switch scheme component

---
+++ src/components/Switch/Scheme.tsx
@@ -0,0 +1,42 @@
+import {
+  Group,
+  Switch,
+  useMantineColorScheme,
+  useMantineTheme,
+} from '@mantine/core';
+import { IconMoonStars, IconSun } from '@tabler/icons-react';
+import { type FC } from 'react';
+
+const SwitchScheme: FC = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const theme = useMantineTheme();
+
+  const SunIcon = (
+    <IconSun size={15} stroke={2.5} color={theme.colors.yellow[4]} />
+  );
+  const MoonIcon = (
+    <IconMoonStars size={15} stroke={2.5} color={theme.colors.blue[6]} />
+  );
+
+  return (
+    <Group position='center' title='Press Ctrl + J'>
+      <Switch
+        size='md'
+        checked={colorScheme === 'dark'}
+        color={colorScheme === 'dark' ? 'gray' : 'dark'}
+        thumbIcon={colorScheme === 'dark' ? SunIcon : MoonIcon}
+        sx={{
+          '& label > *': {
+            backgroundColor: 'unset',
+            border: 'unset',
+          },
+        }}
+        onChange={() => {
+          toggleColorScheme();
+        }}
+        aria-label='Toggle Color Scheme'
+      />
+    </Group>
+  );
+};
+export default SwitchScheme;
---
+++ src/components/Switch/index.ts
@@ -0,0 +1,1 @@
+export { default as SwitchScheme } from './Scheme';
