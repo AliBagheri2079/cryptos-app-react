5d8138b7b3c16bf95b7db11af57b9e153d28b82b
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-23 23:46:49 +0330

    :zap: Perf: improve performance with add shortcut for switch scheme

--- src/components/Switch/Scheme.tsx
+++ src/components/Switch/Scheme.tsx
@@ -4,6 +4,7 @@ import {
   useMantineColorScheme,
   useMantineTheme,
 } from '@mantine/core';
+import { useHotkeys } from '@mantine/hooks';
 import { IconMoonStars, IconSun } from '@tabler/icons-react';
 import { type FC } from 'react';
 
@@ -11,13 +12,21 @@ const SwitchScheme: FC = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const theme = useMantineTheme();
 
+  useHotkeys([
+    [
+      'mod+J',
+      () => {
+        toggleColorScheme();
+      },
+    ],
+  ]);
+
   const SunIcon = (
     <IconSun size={15} stroke={2.5} color={theme.colors.yellow[4]} />
   );
   const MoonIcon = (
     <IconMoonStars size={15} stroke={2.5} color={theme.colors.blue[6]} />
   );
-
   return (
     <Group position='center' title='Press Ctrl + J'>
       <Switch
