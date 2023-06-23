ce28b4aa67eec590db905b3fe7747d09066a36fe
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-23 23:45:34 +0330

    :sparkles: Feat: create toggle language component

---
+++ src/components/Toggle/Language.tsx
@@ -0,0 +1,47 @@
+import { Button } from '@mantine/core';
+import { useHotkeys } from '@mantine/hooks';
+import { IconLanguage } from '@tabler/icons-react';
+import { type FC } from 'react';
+
+import useDirection from '@/hooks/useDirection';
+import useLanguage from '@/hooks/useLanguage';
+
+const ToggleLanguage: FC = () => {
+  const [language, toggleLanguage] = useLanguage();
+  const [, toggleDirection] = useDirection();
+
+  useHotkeys([
+    [
+      'mod+L',
+      () => {
+        toggleDirection();
+        toggleLanguage();
+      },
+    ],
+  ]);
+
+  return (
+    <Button
+      variant='light'
+      radius='xl'
+      size='sm'
+      color='dark'
+      bg='transparent'
+      w='4.5rem'
+      uppercase
+      compact
+      loading={false}
+      loaderPosition='right'
+      rightIcon={<IconLanguage stroke={2.5} />}
+      onClick={() => {
+        toggleDirection();
+        toggleLanguage();
+      }}
+      title='Press Ctrl + L'
+    >
+      {language === 'en' ? 'fa' : 'en'}
+    </Button>
+  );
+};
+
+export default ToggleLanguage;
