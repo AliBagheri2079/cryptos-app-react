8aea9bb4f411e404d497f2e3f544e073e5309f2d
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-07-01 03:12:31 +0330

    :poop: Perf: write some test code to check crypto services

--- src/pages/Crypto.tsx
+++ src/pages/Crypto.tsx
@@ -1,27 +1,90 @@
+import { useShallowEffect } from '@mantine/hooks';
 import { type FC, useEffect } from 'react';
 
-import useAxios from '@/hooks/useAxios';
-import cryptoApi from '@/services/api/crypto';
+import getCoinCandleChart from '@/services/crypto/getCoinCandleChart';
+import getCoinChart from '@/services/crypto/getCoinChart';
+import getCoinHistory from '@/services/crypto/getCoinHistory';
+import getCoinMarket from '@/services/crypto/getCoinMarket';
+import getCoinsMarkets from '@/services/crypto/getCoinsMarkets';
+import searchOnCoingecko from '@/services/crypto/searchOnCoingecko';
 
 const Crypto: FC = () => {
-  const [{ data, status, error, isLoading }, axiosFetch] = useAxios();
-
+  // const { response, statusCode, errorMessage, isLoading } = getCoinsMarkets({
+  const searchData = searchOnCoingecko({
+    query: 'bitcoin',
+  });
   useEffect(() => {
-    const getData = async (): Promise<void> => {
-      await axiosFetch(cryptoApi, {
-        method: 'get',
-        url: '/posts',
-      });
-    };
-    getData();
+    console.log('searchData', searchData);
+  }, [searchData]);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const coinsMarkets = getCoinsMarkets({
+    vs_currency: 'usd',
+    order: 'market_cap_desc',
+    per_page: 250,
+    sparkline: true,
+    price_change_percentage: '24h',
+    locale: 'en',
+    // locale: 'ar',
+  });
+  useEffect(() => {
+    console.log('coinsMarkets', coinsMarkets);
+  }, [coinsMarkets]);
+
+  const coinMarket = getCoinMarket({
+    id: 'bitcoin',
+    community_data: true,
+    developer_data: true,
+    localization: true,
+    market_data: true,
+    sparkline: true,
+    tickers: true,
+  });
+  useEffect(() => {
+    console.log('coinMarket', coinMarket);
+  }, [coinMarket]);
+
+  const coinHistory = getCoinHistory({
+    id: 'bitcoin',
+    date: '30-12-2022',
+    localization: true,
+  });
+  useEffect(() => {
+    console.log('coinHistory', coinHistory);
+  }, [coinHistory]);
+
+  const coinChart = getCoinChart({
+    id: 'bitcoin',
+    vs_currency: 'usd',
+    days: 30,
+    interval: 'daily',
+  });
+  useEffect(() => {
+    console.log('coinChart', coinChart);
+  }, [coinChart]);
+
+  const coinCandleChart = getCoinCandleChart({
+    id: 'bitcoin',
+    vs_currency: 'usd',
+    days: 30,
+  });
+  useEffect(() => {
+    console.log('coinCandleChart', coinCandleChart);
+  }, [coinCandleChart]);
+
+  // useShallowEffect(() => {
+  //   const getData = async (): Promise<void> => {
+  //     await axiosFetch(cryptoApi, {
+  //       method: 'get',
+  //       url: '/posts',
+  //     });
+  //   };
+  //   getData();
+  // }, []);
 
   return (
     <div>
       <h1>Crypto</h1>
-      {isLoading && <p>isLoading...</p>}
+      {/* {isLoading && <p>isLoading...</p>}
 
       {status}
 
@@ -39,7 +102,7 @@ const Crypto: FC = () => {
         <p>{`userId: ${data.data?.userId}, title: ${data.data?.title}, body: ${data.data?.body}`}</p>
       )}
 
-      {!isLoading && !error && !data && <p>No data to display</p>}
+      {!isLoading && !error && !data && <p>No data to display</p>} */}
     </div>
   );
 };
