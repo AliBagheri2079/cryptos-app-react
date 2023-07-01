3cfe6cfa79aa011398748379aba881e3ee6d85d6
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-07-01 04:05:39 +0330

    :art: Refactror: update logo style to fix on directions

--- src/components/Brand/Logo.tsx
+++ src/components/Brand/Logo.tsx
@@ -1,21 +1,36 @@
+import { useMantineTheme } from '@mantine/core';
 import { type FC } from 'react';
 
 const BrandLogo: FC = () => {
+  const theme = useMantineTheme();
+
   return (
     <div className='inline-flex h-[26px] w-[26px] flex-wrap gap-[2px]'>
       <div
-        className='basis-3 rounded-[3.82px] rounded-tr-none bg-current'
+        className='basis-3 rounded-[4px] rounded-tr-none bg-current'
         style={{
-          transform: 'matrix(1, 0, 0, -1, 0, 0)',
+          transform:
+            theme.dir === 'ltr'
+              ? 'matrix(1, 0, 0, -1, 0, 0)'
+              : 'matrix(-1, 0, 0, -1, 0, 0)',
         }}
       />
       <div
-        className='basis-3 rounded-[3.82px] rounded-tr-none bg-current'
+        className='basis-3 rounded-[4px] rounded-tr-none bg-current'
         style={{
-          transform: 'matrix(-1, 0, 0, -1, 0, 0)',
+          transform:
+            theme.dir === 'ltr'
+              ? 'matrix(-1, 0, 0, -1, 0, 0)'
+              : 'matrix(1, 0, 0, -1, 0, 0)',
         }}
       />
-      <div className='basis-3 rounded-[3.82px] rounded-tr-none bg-current' />
+      <div
+        className='basis-3 rounded-[4px] rounded-tr-none bg-current'
+        style={{
+          transform:
+            theme.dir === 'ltr' ? 'unset' : 'matrix(-1, 0, 0, 1, 0, 0)',
+        }}
+      />
     </div>
   );
 };
