4faef734650f3d3ae1eba66a44b2f24e238dd276
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-07-06 22:50:53 +0330

    :art: Refactor: update coins markets services

--- src/services/crypto/getCoinsMarkets.ts
+++ src/services/crypto/getCoinsMarkets.ts
@@ -5,29 +5,28 @@ import cryptoApi from '../api/crypto';
 import useAxios from '@/hooks/useAxios';
 import type CoinsMarketsData from '@/types/Api/Crypto/Data/CoinsMarkets';
 import type CoinsMarketsParams from '@/types/Api/Crypto/Params/CoinsMarkets';
-import type ApiResponse from '@/types/Api/global/Response';
+import type { AxiosResponse } from '@/types/Api/global/UseAxios';
 
 type CoinsMarketsFC = (
   params: CoinsMarketsParams,
-) => ApiResponse<CoinsMarketsData[]>;
+  refetchItems: Array<string | number | undefined>,
+) => AxiosResponse<CoinsMarketsData[]>;
 
-const getCoinsMarkets: CoinsMarketsFC = params => {
-  const [{ response, statusCode, errorMessage, isLoading }, axiosFetch] =
-    useAxios<CoinsMarketsData[]>();
+const getCoinsMarkets: CoinsMarketsFC = (params, refetchItems) => {
+  const [axiosResponse, axiosFetch] = useAxios<CoinsMarketsData[]>();
 
   useEffect(() => {
-    const getData = async (): Promise<void> => {
+    (async () => {
       await axiosFetch(cryptoApi, {
         method: 'get',
         url: '/coins/markets',
         params,
       });
-    };
-    getData();
+    })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [...refetchItems]);
 
-  return { response, statusCode, errorMessage, isLoading };
+  return axiosResponse;
 };
 
 export default getCoinsMarkets;
