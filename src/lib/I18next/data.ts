b749223fd4cf34fbe3f68aa70a61d123aa767a29
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-07-22 01:31:41 +0330

    :sparkles: Feat: add crypto i18next data

--- src/lib/I18next/data.ts
+++ src/lib/I18next/data.ts
@@ -1,6 +1,8 @@
 import { type Resource } from 'i18next';
 
 //* translation catalog
+// TODO: change item to cammel case convention
+// TODO: change to lowercase values
 export const resources: Resource = {
   en: {
     translation: {
@@ -37,6 +39,35 @@ export const resources: Resource = {
       user_avatar_item_1_option_3: 'Logout',
       user_avatar_item_2: 'Danger zone',
       user_avatar_item_2_option_1: 'Delete account',
+
+      // * Crypto Table Header Columns
+      cryptoTableHeaderColumn_1: 'Coin Info',
+      cryptoTableHeaderColumn_2: 'Market Cap',
+      cryptoTableHeaderColumn_3: 'Current Price',
+      cryptoTableHeaderColumn_4: 'Sparkline in last week',
+      cryptoTableHeaderColumn_5: 'Last Updated',
+
+      // * Crypto Table Search Values
+      cryptoTableSearchControler: 'see coin items',
+      cryptoTableSearchPlaceholder: 'search in crypto items',
+      cryptoTableSearchNotFound: 'nothing found ...',
+
+      // * Crypto Table Order Values
+      cryptoTableOrderPlaceholder: 'Pick one',
+      cryptoTableOrderNotFound: 'No options',
+
+      // * Coin Info Table Header Columns
+      coinInfoTableHeaderColumn_1: 'current price',
+      coinInfoTableHeaderColumn_2: 'price change 24h',
+      coinInfoTableHeaderColumn_3: 'highest price 24h',
+      coinInfoTableHeaderColumn_4: 'lowest price 24h',
+      coinInfoTableHeaderColumn_5: 'market cap rank',
+      coinInfoTableHeaderColumn_6: 'market cap',
+      coinInfoTableHeaderColumn_7: 'total volume',
+      coinInfoTableHeaderColumn_8: 'last updated',
+      coinInfoTableHeaderColumn_9: 'coingecko score',
+      coinInfoTableHeaderColumn_10: 'developer score',
+      coinInfoTableHeaderColumn_11: 'community score',
     },
   },
   fa: {
@@ -74,6 +105,35 @@ export const resources: Resource = {
       user_avatar_item_1_option_3: 'خروج',
       user_avatar_item_2: 'بخش خطرناک',
       user_avatar_item_2_option_1: 'حذف اکانت',
+
+      // * عنوان های جدول ارزهای دیجیتال
+      cryptoTableHeaderColumn_1: 'اطلاعات ارز',
+      cryptoTableHeaderColumn_2: 'ارزش بازار',
+      cryptoTableHeaderColumn_3: 'قیمت فعلی',
+      cryptoTableHeaderColumn_4: 'وضعیت هفته گذشته',
+      cryptoTableHeaderColumn_5: 'آخرین بروزرسانی',
+
+      // * مقدارهای جستجوی جدول ارزهای دیجیتال
+      cryptoTableSearchControler: 'جستجو کردن',
+      cryptoTableSearchPlaceholder: 'جستجوی ارز دیجیتال',
+      cryptoTableSearchNotFound: 'گزینه ای وجود ندارد',
+
+      //  * مقدارهای تغییرات جدول ارزهای دیجیتال
+      cryptoTableOrderPlaceholder: 'گزینه ای را انتخاب کن',
+      cryptoTableOrderNotFound: 'گزینه ای وجود ندارد',
+
+      // * عنوان های جدول ارز دیجیتال
+      coinInfoTableHeaderColumn_1: 'قیمت فعلی',
+      coinInfoTableHeaderColumn_2: 'تغییر یک روزه',
+      coinInfoTableHeaderColumn_3: 'بیشترین مقدار رروز',
+      coinInfoTableHeaderColumn_4: 'کمترین مقدار روز',
+      coinInfoTableHeaderColumn_5: 'جایگاه ارزش بازاری',
+      coinInfoTableHeaderColumn_6: 'ارزش بازار',
+      coinInfoTableHeaderColumn_7: 'ارزش کل',
+      coinInfoTableHeaderColumn_8: 'آخرین بروزرسانی',
+      coinInfoTableHeaderColumn_9: 'امتیاز سایت',
+      coinInfoTableHeaderColumn_10: 'امتیاز کدنویس ها',
+      coinInfoTableHeaderColumn_11: 'امتیاز مردمی',
     },
   },
 };
