5665d0712cd7ab8557e444528d5c1892acd66455
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-07-23 01:33:08 +0330

    :sparkles: Feat: create styled component provider

---
+++ src/lib/StyledComponent/theme.ts
@@ -0,0 +1,23 @@
+import { type DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  borderRadius: '0.1rem',
+  fontSize: '1rem',
+
+  sizes: {
+    xs: '0.25rem',
+    sm: '0.5rem',
+    md: '1rem',
+    lg: '2rem',
+    xl: '3rem',
+    xxl: '4rem',
+  },
+
+  colors: {
+    main: '#407bff',
+    primary: '#1a1b1e',
+    secondary: '#ebebeb',
+  },
+};
+
+export default theme;
