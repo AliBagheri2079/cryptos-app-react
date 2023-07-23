4a32cf5e1d188821b8c67495f39f6e77b2d67dec
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-07-24 00:53:13 +0330

    :zap: Perf: improve redirect page performance with add redirect action

--- src/pages/Status/Redirect.tsx
+++ src/pages/Status/Redirect.tsx
@@ -1,9 +1,22 @@
-import { type FC } from 'react';
+import { type FC, useEffect, useRef } from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useNavigate } from 'react-router-dom';
 
 import InProgressTemplate from '@/components/InProgress';
 
 const Redirect: FC = () => {
+  const navigate = useNavigate();
+  const timer = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    timer.current = setTimeout(() => navigate('/dashboard/crypto'), 3000);
+
+    return () => {
+      clearInterval(timer.current as NodeJS.Timeout);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <HelmetProvider>
       <Helmet>
