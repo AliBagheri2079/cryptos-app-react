263032255f86f4bf80d41b0f219da909982a7701
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-07-24 02:38:43 +0330

    :art: Refactor: update many items

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dashboard',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
   },
