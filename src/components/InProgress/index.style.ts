a54707ece1757c3f8243a4bb7900c71d639e4a27
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-07-23 22:30:33 +0330

    :sparkles: Feat: create in progress component

---
+++ src/components/InProgress/index.style.ts
@@ -0,0 +1,36 @@
+import { createStyles, rem } from '@mantine/core';
+
+export const useInProgressStyles = createStyles(theme => ({
+  root: {
+    paddingTop: rem(80),
+    paddingBottom: rem(80),
+  },
+
+  title: {
+    fontWeight: 900,
+    fontSize: rem(34),
+    marginBottom: theme.spacing.md,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: rem(32),
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan('sm')]: {
+      width: '100%',
+    },
+  },
+
+  mobileImage: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  desktopImage: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
