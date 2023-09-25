bf56f8a5b25c507a4b55977a24cace3bc6b87c83
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-09-25 19:31:23 +0330

    :fire: Refactor: remove many unsued components

--- src/components/Search/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CryptoTableSearch } from './CryptoTable';
--- src/components/Table/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CryptoTable } from './Crypto';
-export { default as CoinInfoTable } from './CoinInfo';
-// TODO: move to feature folder
