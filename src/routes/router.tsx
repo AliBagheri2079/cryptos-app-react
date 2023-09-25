a7d66439190082b9c2d7bed9a603fa405a507cd6
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-09-25 19:39:56 +0330

    :art: Refactor: update router file

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,25 +2,15 @@
 import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-import Dashboard from '@/layouts/private/Dashboard';
+import CoinError from '@/pages/Cryptos/Coin/Error';
+import CryptosError from '@/pages/Cryptos/Error';
+import { NotfoundPage, RedirectPage } from '@/pages/status';
 
-// import {
-//   CoinPage,
-//   CoinPageError,
-//   CryptoPage,
-//   NothingFoundPage,
-//   RedirectPage,
-// } from '@/pages';
+const Dashboard = lazy(async () => await import('@/layouts/private/Dashboard'));
+const CryptosPage = lazy(async () => await import('@/pages/Cryptos'));
+const CoinPage = lazy(async () => await import('@/pages/Cryptos/Coin'));
 
-const CoinPage = lazy(async () => await import('@/pages/Coin'));
-const CoinPageError = lazy(async () => await import('@/pages/Coin/Error'));
-const CryptoPage = lazy(async () => await import('@/pages/Crypto'));
-const NothingFoundPage = lazy(
-  async () => await import('@/pages/Status/NothingFound'),
-);
-const RedirectPage = lazy(async () => await import('@/pages/Status/Redirect'));
-
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     children: [
@@ -33,15 +23,24 @@ export const router = createBrowserRouter([
         ),
       },
       {
-        path: 'crypto',
+        path: 'home',
+        element: (
+          <Dashboard>
+            <RedirectPage />
+          </Dashboard>
+        ),
+      },
+      {
+        path: 'cryptos',
         children: [
           {
             index: true,
             element: (
               <Dashboard>
-                <CryptoPage />
+                <CryptosPage />
               </Dashboard>
             ),
+            errorElement: <CryptosError />,
           },
           {
             path: ':id',
@@ -50,23 +49,81 @@ export const router = createBrowserRouter([
                 <CoinPage />
               </Dashboard>
             ),
-            errorElement: <CoinPageError />,
-            // loader: CoinPageLoader,
+            errorElement: <CoinError />,
+            // loader: CoinLoader,
           },
         ],
       },
       {
-        path: '*',
+        path: 'market-news',
         element: (
           <Dashboard>
             <RedirectPage />
           </Dashboard>
         ),
       },
+      {
+        path: 'releases',
+        element: (
+          <Dashboard>
+            <RedirectPage />
+          </Dashboard>
+        ),
+      },
+      {
+        path: 'analytics',
+        element: (
+          <Dashboard>
+            <RedirectPage />
+          </Dashboard>
+        ),
+      },
+      {
+        path: 'contracts',
+        element: (
+          <Dashboard>
+            <RedirectPage />
+          </Dashboard>
+        ),
+      },
+      {
+        path: 'settings',
+        element: (
+          <Dashboard>
+            <RedirectPage />
+          </Dashboard>
+        ),
+      },
+      {
+        path: 'security',
+        element: (
+          <Dashboard>
+            <RedirectPage />
+          </Dashboard>
+        ),
+      },
+      {
+        path: 'account',
+        element: (
+          <Dashboard>
+            <RedirectPage />
+          </Dashboard>
+        ),
+      },
+      {
+        path: 'logout',
+        element: (
+          <Dashboard>
+            <RedirectPage />
+          </Dashboard>
+        ),
+      },
+      {
+        path: '*',
+        element: <NotfoundPage />,
+      },
     ],
   },
-  {
-    path: '*',
-    element: <NothingFoundPage />,
-  },
 ]);
+
+export { router };
